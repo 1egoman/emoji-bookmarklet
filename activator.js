92bbe51af1649e1cf95196eedd89b6d7b279f5c4
1egoman <rsg1egoman@gmail.com>
2016-05-29 09:43:20 -0400

    update local activator uri

--- activator.js
+++ activator.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', encodeURI("https://1egoman.github.io/2-emoji/bundle.js"))
-// xhr.open('GET', encodeURI("127.0.0.1:4000/bundle.js"))
+// xhr.open('GET', encodeURI("127.0.0.1:8000/bundle.js"))
 xhr.onload = function() {
   if (xhr.status === 200) {
     var element = document.createElement("script");
